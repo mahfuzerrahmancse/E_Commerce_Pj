ca7ed8811ece407e4db1de2990ed968c5c885c46
Mahfuzer Rahman Mistu <141608179+mahfuzerrahmancse@users.noreply.github.com>
2023-12-24 18:27:51 +0600

    2nd day

---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import "./Hero.css";
+import hand_icon from "../Assets/hand_icon.png";
+import arrow from "../Assets/arrow.png";
+import hero_image from "../Assets/hero_image.png";
+
+function Hero() {
+  return (
+    <div className="hero" style={{background: "gray"}}>
+      <div className="hero_left">
+        <h2> New Arrivals Only </h2>
+        <div className="hero_hand_icon">
+          <p>New</p>
+          <img src={hand_icon} alt="" />
+        </div>
+        <p> collections </p>
+        <p> for everyone </p>
+      </div>
+      <div className="hero_latest_btn">
+        <div> Latest Collection </div>
+        <img src={arrow} alt="" />
+      </div>
+      <div className="hero_right">
+        <img src={hero_image} alt="" />
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
 
 import logo from "../Assets/logo.png";
 import cart_icon from "../Assets/cart_icon.png";
+import { Link } from "react-router-dom";
+
 
 function Navbar() {
+  const [menu,setMenu]=useState("shop")
   return (
     <>
       <div className="Navbar ">
@@ -13,15 +16,22 @@ function Navbar() {
             <img src={logo} alt="" />
             <p> Shopper </p>
           </div>
-          <div className="NavSon2">NavSon2:</div>
+          <div className="NavSon2">
+            <ul>
+              <li onClick={ ()=>{setMenu("shop")} }> <Link style={ {textDecoration:'none'} } to='/'>Shop</Link > {menu==="shop" ?<hr/> :<></>} </li>
+              <li onClick={ ()=>{setMenu("mens")} }> <Link style={ {textDecoration:'none'} } to='/mens'>Men</Link> {menu==="mens" ?<hr/> :<></>} </li>
+              <li onClick={ ()=>{setMenu("womens")} }> <Link style={ {textDecoration:'none'} } to='/womens'>Women</Link> {menu==="womens" ?<hr/> :<></>} </li>
+              <li onClick={ ()=>{setMenu("kids")} }> <Link style={ {textDecoration:'none'} } to='/kids'>Kids</Link> {menu==="kids" ?<hr/> :<></>} </li>
+            </ul>
+          </div>
           <div className="NavSon3">
-            NavSon3:
-            <button> Login </button>
-            <img src={cart_icon} alt="" />
+            <Link> <button> Login </button> </Link>
+            <Link> <img src={cart_icon} alt="" /> </Link>
+            <div className="count">0</div>
           </div>
         </div>
       </div>
-      <div style={{ minHeight: "700px" }}>gf</div>
+      
     </>
   );
 }
