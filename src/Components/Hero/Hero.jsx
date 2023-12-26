4033feb3b0e85e63577904c41fcf8fd46afb12af
Mahfuzer Rahman Mistu <141608179+mahfuzerrahmancse@users.noreply.github.com>
2023-12-26 17:59:03 +0600

    3rd

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -6,7 +6,7 @@ import hero_image from "../Assets/hero_image.png";
 
 function Hero() {
   return (
-    <div className="hero" style={{background: "gray"}}>
+    <div className="hero" >
       <div className="hero_left">
         <h2> New Arrivals Only </h2>
         <div className="hero_hand_icon">
@@ -15,11 +15,12 @@ function Hero() {
         </div>
         <p> collections </p>
         <p> for everyone </p>
-      </div>
-      <div className="hero_latest_btn">
+        <div className="hero_latest_btn">
         <div> Latest Collection </div>
         <img src={arrow} alt="" />
       </div>
+      </div>
+      
       <div className="hero_right">
         <img src={hero_image} alt="" />
       </div>
